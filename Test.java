e8dbe6a38f5e02781a5e7334a588b70c16a3fada
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-11-27 10:47:36 -0700

    File updated

--- Test.java
+++ Test.java
@@ -1,5 +1,7 @@
 public class Test{
     public static void main(String[] args) {
-        
+        System.out.Println("My name is Adam Zachary");
+        System.out.Println("I am 39 years old");
+        System.out.Println("My hometown is Kuna, ID");
     }
 }
